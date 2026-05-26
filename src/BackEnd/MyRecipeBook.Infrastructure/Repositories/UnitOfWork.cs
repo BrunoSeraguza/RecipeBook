e55eb3697b03b975a6c1c11952317f392876039c
Bruno Seraguza <shzdevops@gmail.com>
2026-05-25 22:53:03 -0300

    feat: add business rules and validate

---
+++ src/BackEnd/MyRecipeBook.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Infrastructure.DataSource;
+
+namespace MyRecipeBook.Infrastructure.Repositories;
+
+internal class UnitOfWork : IUnitOfWork
+{
+    
+    private readonly MyRecipeBookDbContext _dbContext;
+
+    public UnitOfWork(MyRecipeBookDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Commit() =>  await _dbContext.SaveChangesAsync();
+
+}
