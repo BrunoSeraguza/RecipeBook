13910b8b213550bd6076c049b80045dccd52f0fe
Bruno Seraguza <shzdevops@gmail.com>
2026-06-05 19:27:48 -0300

    feat: add tests

---
+++ tests/CommomTestsUtilities/Requests/RequestRegisteredUserJsonBuild.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Request;
+
+namespace CommomTestsUtilities.Requests;
+
+public class RequestRegisteredUserJsonBuild
+{
+
+    public RequestRegisteredUserJson Build()
+    {
+        return new Faker<RequestRegisteredUserJson>()
+            .RuleFor(user => user.Nome,     (f)    => f.Person.FirstName)
+            .RuleFor(user => user.Email,    (f, u) => f.Internet.Email(u.Nome))
+            .RuleFor(user => user.Password, (f)    => f.Internet.Password());
+           
+    }
+}
---
+++ tests/Validators.Test/Users/Register/ValidateRegisterUserTests.cs
@@ -0,0 +1,18 @@
+﻿using CommomTestsUtilities.Requests;
+using MyRecipeBook.Application.UseCases.Users.Register;
+
+namespace Validators.Test.Users.Register;
+
+public class ValidateRegisterUserTests
+{
+    [Fact]
+    public  void Success()
+    {
+        var validade = new ValidateRegisterUser();
+        var request = new RequestRegisteredUserJsonBuild();
+
+        var result = validade.Validate(request.Build());
+
+        Assert.True(result.IsValid);
+    }
+}
