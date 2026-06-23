2f4b0eb415b359f32bf4eb3cd377cac5db032802
Bruno Seraguza <shzdevops@gmail.com>
2026-06-22 22:22:04 -0300

    feat: add database in memory for register user test

--- tests/WebApi.Test/User/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -6,11 +6,11 @@ using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace WebApi.Test.User.Register;
 
-public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
+public class RegisterUserTest : IClassFixture< CustomWebApplicationFactory>
 {
     private readonly HttpClient _httpClient;
 
-    public RegisterUserTest(WebApplicationFactory<Program> factory)
+    public RegisterUserTest(CustomWebApplicationFactory factory)
     {
         _httpClient = factory.CreateClient();
     }
